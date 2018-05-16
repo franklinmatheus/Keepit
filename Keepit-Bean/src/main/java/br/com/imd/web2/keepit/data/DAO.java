064ede56e63f4b1773986145c1435c15dfa33177
Franklin Matheus <franklinmatheus1@gmail.com>
2018-05-16 01:30:59 -0300

    initial project structure (only classes definition)

---
+++ Keepit-Bean/src/main/java/br/com/imd/web2/keepit/data/DAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.imd.web2.keepit.data;
+
+/**
+ *
+ * @author franklin
+ * @param <Type>
+ */
+public interface DAO<Type> {
+    
+}
---
+++ Keepit-Bean/src/main/java/br/ufrn/imd/web2/keepit/entity/ClasseBaixa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufrn.imd.web2.keepit.entity;
+
+/**
+ *
+ * @author franklin
+ */
+public class ClasseBaixa extends ClasseSocial {
+    
+}

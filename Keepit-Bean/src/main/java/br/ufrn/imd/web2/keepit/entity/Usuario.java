d651fde8e5dbd87555ef946f06daa151fc1aa375
Franklin Matheus <franklinmatheus1@gmail.com>
2018-05-16 01:50:38 -0300

    user entity and persistensce unit added

--- Keepit-Bean/src/main/java/br/ufrn/imd/web2/keepit/entity/Usuario.java
+++ Keepit-Bean/src/main/java/br/ufrn/imd/web2/keepit/entity/Usuario.java
@@ -5,10 +5,119 @@
  */
 package br.ufrn.imd.web2.keepit.entity;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author franklin
  */
-public class Usuario {
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
     
+    @Column(name = "nome")
+    private String nome;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+    
+    @Column(name = "saldo")
+    private double saldo;
+    
+    @OneToMany
+    private List<Despesa> despesas = new ArrayList<>();
+    
+    @OneToMany
+    private List<Receita> receitas = new ArrayList<>();
+    
+    public Usuario() { }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Despesa> getDespesas() {
+        return despesas;
+    }
+
+    public void setDespesas(List<Despesa> despesas) {
+        this.despesas = despesas;
+    }
+
+    public List<Receita> getReceitas() {
+        return receitas;
+    }
+
+    public void setReceitas(List<Receita> receitas) {
+        this.receitas = receitas;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
 }
